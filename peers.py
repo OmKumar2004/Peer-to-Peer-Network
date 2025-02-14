e57b212f28b2e26c372d65e1570c2b9aeaa58f39
GhostByte <184267947+0xkltz@users.noreply.github.com>
2025-02-15 01:43:42 +0530

    Fixed data transfer and to be implemented the sending the peer's server ip to seed

--- peers.py
+++ peers.py
@@ -1,6 +1,7 @@
 import random
 import socket
 import threading
+from typing import List
 import time
 
 class Peers:
@@ -12,13 +13,13 @@ class Peers:
         
         self.seed_list = []
         self.peer_list = []
-        self.seed_connections = []       # List of sockets of connections when it is behaving as client
+        self.seed_connections: List[socket.socket] = []       # List of sockets of connections when it is behaving as client
         self.peer_connections = []
         
     def creation(self):  # activate (fulfiling it as server)
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.ip, self.port))
-        self.server_socket.listen() 
+        self.server_socket.listen(100) 
         thread = threading.Thread(target=self.accept_connections, daemon=True)
         thread.start()
         print(f"Peer listening on {self.ip}:{self.port}")
@@ -31,9 +32,15 @@ class Peers:
         for seed in self.seed_list:
             self.connect_to_seed(seed)
         
+        print("hi there")
         # Requesting the peer list from all connected seeds 
         self.request_peer_lists()
+        print("hi 2")
+        for peer in self.peer_list:
+            print(peer)
+        print("hi 3")
         self.connect_to_peers()
+        print("hi 4")
         
 
     
@@ -50,7 +57,7 @@ class Peers:
                 break
             
             
-    def handle_peer_connection(self, connection, address):
+    def handle_peer_connection(self, connection: socket.socket, address: tuple):
         while True:
             try:
                 data = connection.recv(1024)
@@ -78,11 +85,15 @@ class Peers:
 
 
     def request_peer_lists(self):
-        self.peer_list = []
+                
         for seed_socket in self.seed_connections:
             try:
-                seed_socket.send(b"REQUEST_PEER_LIST")
+                print("hi 5")
+                seed_socket.sendall(b"REQUEST_PEER_LIST")
+                print("hi 6")
                 peer_list_str = seed_socket.recv(1024)
+                peer_list_str = peer_list_str.decode('utf-8')
+                print("hi 7")
                 # Split the received string into individual peer entries
                 if peer_list_str:
                     peers = peer_list_str.split('\n')
@@ -96,16 +107,18 @@ class Peers:
         
 
     def connect_to_peers(self):     # Acting as client
-        for peer in self.peer_list:
+        for peer in set(self.peer_list):
             try:
                 peer_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                print(peer[0],"  ", peer[1])
                 peer_socket.connect((peer[0], peer[1]))
+                print("p1")
                 self.peer_connections.append(peer_socket)
-                print(f"Peer(client)({self.ip}:{self.port}) -> Connected to {peer[0]}:{peer[1]}")
+                print(f"Peer(client)({self.ip}:{self.port}) -> Connected to peer {peer[0]}:{peer[1]}")
                 peer_socket.sendall(b"Hello from Peer(client)")
                 
             except socket.error as e:
-                print(f"Peer(client)({self.ip}:{self.port}) -> Failed to connect to {peer[0]}:{peer[1]}. Error:{e}")
+                print(f"Peer(client)({self.ip}:{self.port}) -> Failed to connect to peer {peer[0]}:{peer[1]}. Error:{e}")
 
     def close(self):
         if self.server_socket:
